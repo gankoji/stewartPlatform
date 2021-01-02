5f42351ceec942214313be659174b7df9bf45c7c
Jake Bailey <asaxplayinghorse@gmail.com>
2021-01-02 10:14:13 -0700

    Fleshing out the model.

--- StewartDynamics.py
+++ StewartDynamics.py
@@ -5,22 +5,39 @@ from sympy.utilities.lambdify import lambdify
 
 
 ## Generating the equations of motion of a Gough-Stewart Platform system
-q1 = dynamicsymbols('q1')
-q1d = dynamicsymbols('q1', 1)
-u1 = dynamicsymbols('u1')
-u1d = dynamicsymbols('u1', 1)
-l, m, g = symbols('l m g')
 
+## Generalized coordinates and speeds
+## Here we use the length of the 6 actuators
+q1, q2, q3, q4, q5, q6 = dynamicsymbols('q1 q2 q3 q4 q5 q6')
+q1d, q2d, q3d, q4d, q5d, q6d = dynamicsymbols('q1 q2 q3 q4 q5 q6', 1)
+u1, u2, u3, u4, u5, u6 = dynamicsymbols('u1 u2 u3 u4 u5 u6')
+u1d, u2d, u3d, u4d, u5d, u6d = dynamicsymbols('u1 u2 u3 u4 u5 u6', 1)
+
+## Platform parameters
+h, m, g = symbols('h m g')
+Ixx, Iyy, Izz = symbols('Ixx Iyy Izz')
+
+## Attachment points of actuators on moving platform
+x1, y1 = symbols('x1 y1')
+x2, y2 = symbols('x2 y2')
+x3, y3 = symbols('x3 y3')
+x4, y4 = symbols('x4 y4')
+x5, y5 = symbols('x5 y5')
+x6, y6 = symbols('x6 y6')
+
+## Geometry specification
 N = ReferenceFrame('N')
-
 O = Point('O')
-P = O.locatenew('P', l*N.x)
 O.set_vel(N, 0)
-P.set_vel(N, u1*N.x)
 
+B = ReferenceFrame('B')
+
+## Center of mass of moving platform
+P = O.locatenew('P', h*N.z)
+#P.set_vel(N, u1*N.x)
 ParP = Particle('ParP', P, m)
 
-kd = [q1d - u1]
+kd = [q1d - u1, q2d - u2, q3d - u3, q4d - u4, q5d - u5, q6d - u6]
 FL = [(P, m*g*N.x)]
 BL = [ParP]
 
